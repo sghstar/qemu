274ba4f2a680e547eec3ded557f57e906ad44066
Rex Zhuo <rex.zhuo@outlook.com>
2018-11-02 22:37:44 +0800

    PLMT: update implementation to self contain

--- hw/riscv/andes_plmt.c
+++ hw/riscv/andes_plmt.c
@@ -3,43 +3,199 @@
  *
  * Copyright (c) 2018 Andes Tech. Corp.
  *
- * This provides real-time clock, timer and interprocessor interrupts.
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2 or later, as published by the Free Software Foundation.
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
  *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
- * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "qemu/osdep.h"
 #include "qemu/error-report.h"
+#include "qemu/timer.h"
+#include "target/riscv/cpu.h"
 #include "hw/sysbus.h"
-#include "hw/riscv/sifive_clint.h"
 #include "hw/riscv/andes_plmt.h"
+
+static uint64_t cpu_riscv_read_rtc(void)
+{
+    return muldiv64(qemu_clock_get_ns(QEMU_CLOCK_VIRTUAL),
+        ANDES_PLMT_TIMEBASE_FREQ, NANOSECONDS_PER_SECOND);
+}
+
+/*
+ * Called when timecmp is written to update the QEMU timer or immediately
+ * trigger timer interrupt if mtimecmp <= current timer value.
+ */
+static void andes_plmt_write_timecmp(RISCVCPU* cpu, uint64_t value)
+{
+    uint64_t next;
+    uint64_t diff;
+
+    uint64_t rtc_r = cpu_riscv_read_rtc();
+
+    cpu->env.timecmp = value;
+    if (cpu->env.timecmp <= rtc_r) {
+        /* if we're setting an MTIMECMP value in the "past",
+           immediately raise the timer interrupt */
+        riscv_set_local_interrupt(cpu, MIP_MTIP, 1);
+        return;
+    }
+
+    /* otherwise, set up the future timer interrupt */
+    riscv_set_local_interrupt(cpu, MIP_MTIP, 0);
+    diff = cpu->env.timecmp - rtc_r;
+    /* back to ns (note args switched in muldiv64) */
+    next = qemu_clock_get_ns(QEMU_CLOCK_VIRTUAL) + muldiv64(diff, NANOSECONDS_PER_SECOND, ANDES_PLMT_TIMEBASE_FREQ);
+    timer_mod(cpu->env.timer, next);
+}
+
+/*
+ * Callback used when the timer set using timer_mod expires.
+ * Should raise the timer interrupt line
+ */
+static void andes_plmt_timer_cb(void* opaque)
+{
+    RISCVCPU* cpu = opaque;
+    riscv_set_local_interrupt(cpu, MIP_MTIP, 1);
+}
+
+static uint64_t andes_plmt_read(void* opaque, hwaddr addr, unsigned size)
+{
+    AndesPLMTState* plmt = opaque;
+    uint64_t rz;
+    switch (addr) {
+    case 0: /* mtime */
+        rz = cpu_riscv_read_rtc() & 0xFFFFFFFFu;
+        break;
+    case 4: /* mtimeh */
+        rz = (cpu_riscv_read_rtc() >> 32) & 0xFFFFFFFFu;
+        break;
+    case 8: /* mtimecmp */
+    case 12: /* mtimecmph */
+    {
+        size_t hartid = plmt->target;
+        CPUState* cpu = qemu_get_cpu(hartid);
+        CPURISCVState* env = cpu ? cpu->env_ptr : NULL;
+        if (!env) {
+            error_report("plmt: invalid timecmp hartid: %zu", hartid);
+        } else if (addr == 8) {
+            rz = env->timecmp & 0xFFFFFFFFu;
+        } else if (addr == 12) {
+            rz = (env->timecmp >> 32) & 0xFFFFFFFFu;
+        } else {
+            error_report("plmt: invalid read: %08x", (uint32_t)addr);
+            return 0;
+        }
+        break;
+    }
+    default:
+        rz = 0;
+        error_report("plmt: invalid read: %08x", (uint32_t)addr);
+    }
+
+    return rz;
+}
+
+static void andes_plmt_write(void* opaque, hwaddr addr, uint64_t value, unsigned size)
+{
+    AndesPLMTState* plmt = opaque;
+
+    switch (addr) {
+    case 0: /* mtime */
+        error_report("plmt: mtime write not implemented");
+        break;
+    case 4: /* mtimeh */
+        error_report("plmt: mtimeh write not implemented");
+        break;
+    case 8: /* mtimecmp */
+    case 12: /* mtimecmph */
+    {
+        size_t hartid = plmt->target;
+        CPUState* cpu = qemu_get_cpu(hartid);
+        CPURISCVState* env = cpu ? cpu->env_ptr : NULL;
+        if (!env) {
+            error_report("plmt: invalid timecmp hartid: %zu", hartid);
+        } else if (addr == 8) {
+            uint64_t timecmp_hi = env->timecmp >> 32;
+            andes_plmt_write_timecmp(RISCV_CPU(cpu), (timecmp_hi << 32) | (value & 0xFFFFFFFFu));
+        } else if (addr == 12) {
+            uint64_t timecmp_lo = env->timecmp;
+            andes_plmt_write_timecmp(RISCV_CPU(cpu), (value << 32) | (timecmp_lo & 0xFFFFFFFFu));
+        } else {
+            error_report("plmt: invalid timecmp write: %08x", (uint32_t)addr);
+        }
+        break;
+    }
+    default:
+        error_report("plmt: invalid write: %08x", (uint32_t)addr);
+    }
+}
+
+static const MemoryRegionOps andes_plmt_ops = {
+    .read = andes_plmt_read,
+    .write = andes_plmt_write,
+    .endianness = DEVICE_LITTLE_ENDIAN,
+    .valid = {
+        .min_access_size = 4,
+        .max_access_size = 4 }
+};
+
+static Property andes_plmt_properties[] = {
+    DEFINE_PROP_UINT32("target", AndesPLMTState, target, 0),
+    DEFINE_PROP_END_OF_LIST(),
+};
+
+static void andes_plmt_realize(DeviceState* dev, Error** errp)
+{
+    AndesPLMTState* s = ANDES_PLMT(dev);
+    memory_region_init_io(&s->mmio, OBJECT(dev), &andes_plmt_ops, s,
+        TYPE_ANDES_PLMT, ANDES_PLMT_MMIO_SIZE);
+    sysbus_init_mmio(SYS_BUS_DEVICE(dev), &s->mmio);
+}
+
+static void andes_plmt_class_init(ObjectClass* klass, void* data)
+{
+    DeviceClass* dc = DEVICE_CLASS(klass);
+    dc->realize = andes_plmt_realize;
+    dc->props = andes_plmt_properties;
+}
+
+static const TypeInfo andes_plmt_info = {
+    .name = TYPE_ANDES_PLMT,
+    .parent = TYPE_SYS_BUS_DEVICE,
+    .instance_size = sizeof(AndesPLMTState),
+    .class_init = andes_plmt_class_init,
+};
+
+static void andes_plmt_register_types(void)
+{
+    type_register_static(&andes_plmt_info);
+}
+
+type_init(andes_plmt_register_types)
 
 /*
  * Create PLMT device.
  */
-DeviceState *
-andes_plmt_create(hwaddr addr, hwaddr size, uint32_t num_harts,
-                  uint32_t time_base, uint32_t timecmp_base)
+DeviceState* andes_plmt_create(hwaddr addr, hwaddr size, uint32_t target)
 {
-    DeviceState *dev;
+    CPUState* cpu = qemu_get_cpu(target);
+    CPURISCVState* env = cpu ? cpu->env_ptr : NULL;
+    if (env) {
+        env->timer = timer_new_ns(QEMU_CLOCK_VIRTUAL, &andes_plmt_timer_cb, cpu);
+        env->timecmp = 0;
+    }
 
-    dev = sifive_clint_create(addr, size, num_harts,  ANDES_SIP_BASE, timecmp_base, time_base);
-
+    DeviceState* dev = qdev_create(NULL, TYPE_ANDES_PLMT);
+    qdev_prop_set_uint32(dev, "target", target);
+    qdev_init_nofail(dev);
+    sysbus_mmio_map(SYS_BUS_DEVICE(dev), 0, addr);
     return dev;
 }
--- include/hw/riscv/andes_plmt.h
+++ include/hw/riscv/andes_plmt.h
@@ -3,29 +3,21 @@
  *
  * Copyright (c) 2018 Andes Tech. Corp.
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2 or later, as published by the Free Software Foundation.
  *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
- * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef HW_ANDES_PLMT_H
 #define HW_ANDES_PLMT_H
-
-#include "hw/riscv/sifive_clint.h"
 
 #define TYPE_ANDES_PLMT "riscv.andes.plmt"
 
@@ -33,17 +25,22 @@
     OBJECT_CHECK(AndesPLMTState, (obj), TYPE_ANDES_PLMT)
 
 typedef struct AndesPLMTState {
-    SiFiveCLINTState state;
+    /*< private >*/
+    SysBusDevice parent_obj;
+
+    /*< public >*/
+    MemoryRegion mmio;
+    uint32_t target;
+    uint32_t time_base;
+    uint32_t timecmp_base;
 } AndesPLMTState;
 
 DeviceState *
-andes_plmt_create(hwaddr addr, hwaddr size, uint32_t num_harts,
-                  uint32_t time_base, uint32_t timecmp_base);
+andes_plmt_create(hwaddr addr, hwaddr size, uint32_t target);
 
 enum {
-    ANDES_TIME_BASE    = 0x0000,
-    ANDES_TIMECMP_BASE = 0x0008,
-    ANDES_SIP_BASE     = 0x1000, /* virtual! */
+    ANDES_PLMT_MMIO_SIZE = 0x100000,
+    ANDES_PLMT_TIMEBASE_FREQ = 60 * 1000 * 1000
 };
 
 #endif
